48084f5b686adfdcc5a9fcd64060a4db808b0730
William Gusarov <67597085+willg343@users.noreply.github.com>
2022-09-23 20:12:58 -0300

    Create Source.c

---
+++ C 26.08.2022/Source.c
@@ -0,0 +1,16 @@
+/*
+  The source code is authored by the following CodeWars user: www.codewars.com/users/porkfreezer
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+char *find_the_number_plate(int id) {
+  char *out = calloc(7, 1);
+  int sn = id % 999 + 1;
+  char l1 = (id /= 999) % 26 + 'a';
+  char l2 = (id /= 26) % 26 + 'a';
+  char l3 = (id / 26) % 26 + 'a';
+  sprintf(out, "%c%c%c%03d", l1, l2, l3, sn);
+  return out;
+}
