0f1c73c2d016a7a058088264298be5248ec94623
William Gusarov <67597085+willg343@users.noreply.github.com>
2022-09-23 21:10:28 -0300

    Update SampleTests.c

--- RISC-V 01.09.2022/SampleTests.c
+++ RISC-V 01.09.2022/SampleTests.c
@@ -1,5 +1,5 @@
 /*
-  The sample test suite was developed by the following CodeWars user: www.codewars.com/users/donaldsebleung
+  The sample test suite is developed by the following CodeWars user: www.codewars.com/users/donaldsebleung
 */
 
 
