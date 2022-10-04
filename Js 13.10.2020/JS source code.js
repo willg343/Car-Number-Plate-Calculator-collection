2386bc65b50bd5fe0eb00a25d00c53462af8807b
William Gusarov <67597085+willg343@users.noreply.github.com>
2022-10-04 12:24:58 -0300

    Update JS source code.js

--- Js 13.10.2020/JS source code.js
+++ Js 13.10.2020/JS source code.js
@@ -1,7 +1,8 @@
+const alph = "abcdefghijklmnopqrstuvwxyz"
+
 function findTheNumberPlate(n){
-    var a = 'abcdefghijklmnopqrstuvwxyz'
     var right = Math.floor(Math.floor(n/999)/676)
-    var middle = Math.floor((Math.floor(n/999)-676*l1)/26)
-    var left = Math.floor(n/999)-l1*676-l2*26
-    return `${a[left]}${a[middle]}${a[right]}` + (''+(n%999+1)).padStart(3,'0')
+    var middle = Math.floor((Math.floor(n/999)-676*right)/26)
+    var left = Math.floor(n/999)-right*676-middle*26
+    return `${alph[left]}${alph[middle]}${alph[right]}` + (''+(n%999+1)).padStart(3,'0')
 }
