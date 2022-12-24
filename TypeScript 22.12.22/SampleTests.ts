c0dd80a57d733bcfca18abe80cb3ae9adc442628
William Gusarov <67597085+willg343@users.noreply.github.com>
2022-12-23 23:10:33 -0400

    Create SampleTests.ts

---
+++ TypeScript 22.12.22/SampleTests.ts
@@ -0,0 +1,20 @@
+import { assert } from "chai";
+import { findTheNumberPlate } from "./solution";
+
+var tests : [number, string][] =[
+          [3,'aaa004'],
+          [1487,'baa489'],
+          [40000,'oba041'],
+          [17558423,'zzz999'],
+          [234567,'aja802'],
+          [43056, 'rba100']]
+
+
+describe("Sample Test Cases", function()  {
+  tests.forEach( function (test:[number, string]): void {
+    it(`Testing for ${test[0]}`, function() {
+      console.log(test[0], test[1])
+      assert.strictEqual(findTheNumberPlate(test[0]), test[1]);
+    });
+  });
+});
